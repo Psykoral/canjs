2121587d50a4700c1b3cfb1e015e29e9ad2897bc
jisutton <jisutton@adobe.com>
2015-11-13 17:32:11 -0800

    Fixing can.js references

--- view/stache/stache_test.js
+++ view/stache/stache_test.js
@@ -1,5 +1,5 @@
 /* jshint asi:true,multistr:true,indent:false,latedef:nofunc*/
-steal("can/util/can", "can-simple-dom", "can/util/vdom/build_fragment","can/view/stache", "can/view",
+steal("can/util/can.js", "can-simple-dom", "can/util/vdom/build_fragment","can/view/stache", "can/view",
 	"can/test","can/view/mustache/spec/specs","steal-qunit",
 	"can/view/stache/expression_test.js","can/view/stache/mustache_helpers.js",
 	function(can){
